325ea98d0020fead34332977daf723c4e547a9aa
GirishCodeAlchemy <venkatareddygirish@gmail.com>
2024-06-12 09:38:21 -0400

    Fix the black

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,7 +7,9 @@ import pytest
 
 
 def test_version():
-    result = subprocess.run([sys.executable, "-m", "alchemy.cli", "version"], capture_output=True, text=True)
+    result = subprocess.run(
+        [sys.executable, "-m", "alchemy.cli", "version"], capture_output=True, text=True
+    )
     assert result.returncode == 0
     version_pattern = r"^\d+\.\d+\.\d+(\.dev\d+)?$"
     result = result.stdout.strip()
@@ -16,28 +18,49 @@ def test_version():
 
 
 def test_export_data():
-    result = subprocess.run([
-        sys.executable, "-m", "alchemy.cli", "export_data",
-        "--mode", "csv",
-        "--meta_data_xls", "metadata.xlsx",
-        "--output_path", "output/"
-    ], capture_output=True, text=True)
+    result = subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "alchemy.cli",
+            "export_data",
+            "--mode",
+            "csv",
+            "--meta_data_xls",
+            "metadata.xlsx",
+            "--output_path",
+            "output/",
+        ],
+        capture_output=True,
+        text=True,
+    )
     assert result.returncode == 0
     assert "Invoke export code" in result.stdout
 
 
 def test_filter_data():
-    result = subprocess.run([
-        sys.executable, "-m", "alchemy.cli", "filter_data",
-        "--mode", "csv",
-        "--meta_data_xls", "metadata.xlsx"
-    ], capture_output=True, text=True)
+    result = subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "alchemy.cli",
+            "filter_data",
+            "--mode",
+            "csv",
+            "--meta_data_xls",
+            "metadata.xlsx",
+        ],
+        capture_output=True,
+        text=True,
+    )
     assert result.returncode == 0
     print(result.stdout)
     assert "Invoke filter" in result.stdout
 
 
 def test_unknown_command():
-    result = subprocess.run([sys.executable, "-m", "alchemy.cli", "unknown"], capture_output=True, text=True)
+    result = subprocess.run(
+        [sys.executable, "-m", "alchemy.cli", "unknown"], capture_output=True, text=True
+    )
     assert result.returncode == 2
     assert "" == result.stdout
